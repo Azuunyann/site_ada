29b36eaceaca3b6c3564324f2e2c67efbcc478f3
Jade <jadecavalcanti5@gmail.com>
2025-03-11 11:23:56 -0300

    Primeiro Commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.getElementById("botaoEnviar").addEventListener("click", valida_formulario)
+
+function valida_formulario(){
+    if(document.getElementById("nome-da-usuaria").value != "" && 
+        document.getElementById("email-da-usuaria").value != "" && 
+        document.getElementById("telefone-da-usuaria").value != ""){
+            alert("Prontinho! Você receberá as novidades por email.")
+        }else{
+            alert("Por favor, preencha os campos nome, email e número de telefone!")
+        }
+    }
